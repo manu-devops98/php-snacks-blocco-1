0f002e14e7790a1405a46b70b7f58ef82371b7df
manu-devops98 <manuelcappello7@gmail.com>
2022-01-31 16:19:51 +0100

    add snack-4

--- snack4.php
+++ snack4.php
@@ -1,1 +1,21 @@
 <!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+<?php
+  $paragraph = ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea facere ipsum odit suscipit atque sapiente enim. Quasi soluta dolor delectus excepturi vero dolores. Quisquam, quam? Atque qui fugiat deleniti quod!  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea facere ipsum odit suscipit atque sapiente enim. Quasi soluta dolor delectus excepturi vero dolores. Quisquam, quam? Atque qui fugiat deleniti quod!';
+  $newParagraph = explode('.', $paragraph);
+    foreach ($newParagraph as $singleParagraph) {
+        echo '<p> Paragrafo suddiviso:' . $singleParagraph . '</p>';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Paragrafo: <?php echo $paragraph; ?></h1>
+</body>
+</html>
