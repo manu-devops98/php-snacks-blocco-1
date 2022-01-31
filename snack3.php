2a678b4230a979dff6aa1825ca26c02fbe67bf01
manu-devops98 <manuelcappello7@gmail.com>
2022-01-31 16:09:31 +0100

    ad snack-3

--- snack3.php
+++ snack3.php
@@ -1,1 +1,11 @@
 <!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+<?php
+ $numberRand = [];
+ while (count($numberRand) < 15) {
+    $num = rand(1,50);
+    if (!in_array($num, $numberRand)) {
+       $numberRand[] = $num; 
+    }
+ }
+ var_dump($numberRand);
+?>
